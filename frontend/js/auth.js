7ee42a3ed892a0e8a2c30b3f2b7ca31acf9890e5
Jami-008 <jsiddiquiofficial@gmail.com>
2026-07-22 02:29:06 +0600

    Phase 2: Initial frontend foundation

---
+++ frontend/js/auth.js
@@ -0,0 +1,18 @@
+const password = document.getElementById("password");
+const toggle = document.getElementById("togglePassword");
+
+toggle.addEventListener("click", () => {
+
+    if(password.type === "password"){
+
+        password.type = "text";
+        toggle.textContent = "🙈";
+
+    }else{
+
+        password.type = "password";
+        toggle.textContent = "👁";
+
+    }
+
+});
